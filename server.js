2d679c76d3f14e2fee75ced42206cdf1a011f655
dshaw1242 <david.shaw1242@gmail.com>
2021-06-11 07:52:27 -0600

    updated server.js with a session to update some model needs for later

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: process.env.COOKIE,
+    cookie: {},
+    resave: false,
+    saveUnitialized: false,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 app.use(routes)
+
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars');
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
