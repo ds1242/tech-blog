bda1b50c969c4c2a2e7666d8c4e3cadfa2ad13c5
dshaw1242 <david.shaw1242@gmail.com>
2021-06-12 14:39:24 -0600

    posts load as cards on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,6 @@ router.get('/', (req, res) => {
             posts,
             loggedIn: req.session.loggedIn
         });
-        // res.render('homepage', { posts });
     })
     .catch(err => {
         res.status(500).json({ message: 'Something went wrong loading page' })
