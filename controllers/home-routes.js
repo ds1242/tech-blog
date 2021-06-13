e486a7c89c23b9b3076f892a43c46bde65a63cc8
dshaw1242 <david.shaw1242@gmail.com>
2021-06-13 11:35:14 -0600

    added single post handlebars and abilty to open and view the post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -44,4 +44,43 @@ router.get('/login', (req, res) => {
     res.render('login');
 })
 
+
+router.get('/posts/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_text', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbSinglePostData => {
+        if(!dbSinglePostData) {
+            res.status(404).json({ message: 'Unable to find a post with that id' });
+            return;
+        }
+        const post = dbSinglePostData.get({ plain: true });
+
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'something went wrong opening that post' });
+    })
+})
+
 module.exports = router;
